e07cb492b55a66f1c971cfc823c5141171692b5d
IsiomaFON <93886915+IsiomaFON@users.noreply.github.com>
2022-09-01 16:50:27 -0600

    Update test.py
     Code to collect:
    - list of all the motions
        - title of the motion
        - who it was moved by
        - other details
        - whether or not the motion was carried
        - links to any attachments
    
    Work in progress...

--- test.py
+++ test.py
@@ -45,3 +45,43 @@ also_present = [x.text for x in attendance_table[5].find_all("li")]
 
 ## Body information
 
+### Code to collect:
+# - list of all the motions
+#     - title of the motion
+#     - who it was moved by
+#     - other details
+#     - whether or not the motion was carried
+#     - links to any attachments
+
+# Select the section of the HTML with the meeting details
+section_1= soup.select('div.AgendaItems > div.AgendaItemContainer')
+
+# Create nested dictionary
+Result = defaultdict(dict)
+
+# Iterate through page HTML to extract information
+for i, each_section in enumerate(section_1):
+    
+    # For items without sub motions
+    if each_section.findChildren('div', class_="AgendaItemContainer") == []:   
+        # Extract title of motion
+        Result[i]['Title of motion'] = [each_section.find('a').text]
+        # Extract Moved by
+        Result[i]['Moved by'] = [x.get_text() for x in each_section.select('div.MovedBy > span.Value')] #.find('div', class_='MovedBy').find('span', class_='Value')
+        # Extract Details of motion
+        Result[i]['Details'] = [x.get_text() for x in each_section.find_all('p')]
+        # Extract status of motion (was motion carried?)
+        Result[i]['Status of Motion'] = [x.get_text() for x in each_section.select('div.MotionResult')]  #find('div', {'class':"MotionResult"}).text]
+        # Get JS item number for attachment link
+        attachment_num_list = [re.findall('[0-9]+', x.get('href'))[0] for x in each_section.find_all('a', href=re.compile('attachments'))]
+        for num in attachment_num_list:
+            attachment_num = num
+        # Extract attachment links 
+        attachment_list=[]
+        for element in set(soup.find_all('div', class_= re.compile('AgendaItemAttachment AgendaItemAttachment'+attachment_num))):    
+            attachment_list.append('https://pub-calgary.escribemeetings.com/'+element.find('a')['href'])
+        Result[i]['Attachments'] = attachment_list
+
+    # For items with sub motions
+    else:
+        pass
